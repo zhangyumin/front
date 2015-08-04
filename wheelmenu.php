2a51a3887dcfa036146149b5788c27f8a04e5784
zhangyumin <zhangyumin618@163.com>
2015-08-04 19:48:37 +0800

    confirm the icon

--- wheelmenu.php
+++ wheelmenu.php
@@ -1,30 +1,26 @@
 <style>
-    .main{position: fixed;z-index: 1000;bottom: 30px;right:0;}
+    .main{position: fixed;z-index: 1000;bottom: 20px;right:20px;}
 </style>
 <link rel="stylesheet" type="text/css" href="./src/jquery-wheelmenu/css/default.css" />
-<script type="text/javascript" src="./src/jquery-2.0.0.min.js"></script>
+<!--<script type="text/javascript" src="./src/jquery-2.0.0.min.js"></script>-->
 <script type='text/javascript' src='./src/jquery-wheelmenu/js/jquery.wheelmenu.js'></script>
 <link rel='stylesheet' type='text/css' href='./src/jquery-wheelmenu/css/style.css'/>
 <link href='./src/jquery-wheelmenu/css/elusive-webfont.css' rel='stylesheet' type='text/css'/>
 
 <div class="main">
-    <a href="#wheel" class="wheel-button">
+    <a href="#wheel" class="wheel-button nw">
         <i class="customicon-plus"></i>
     </a>
-    <div class="pointer">Click me</div>
-        <ul id="wheel">
-            <li class="item"><a href="#home"><i class="customicon-home"></i></a></li>
-            <li class="item"><a href="#home"><i class="customicon-music"></i></a></li>
-            <li class="item"><a href="#home"><i class="customicon-video"></i></a></li>
-            <li class="item"><a href="#home"><i class="customicon-heart"></i></a></li>
-            <li class="item"><a href="#home"><i class="customicon-user"></i></a></li>
-            <li class="item"><a href="#home"><i class="customicon-trash"></i></a></li>
-            <li class="item"><a href="#home"><i class="customicon-envelope"></i></a></li>
-            <li class="item"><a href="#home"><i class="customicon-camera"></i></a></li>
-            <li class="item"><a href="#home"><i class="customicon-picture"></i></a></li>
-            <li class="item"><a href="#home"><i class="customicon-eye-open"></i></a></li>
+        <ul id="wheel" data-angle="NW" class="wheel">
+            <li class="item"><a href="./task_summary.php"><i class="customicon-file-edit"></i></a></li>
+            <li class="item"><a href="./show_result.php"><i class="customicon-picture"></i></a></li>
+            <li class="item"><a href="./show_sequence_searched.php?chr=1&gene=31185&strand=-1"><i class="customicon-search"></i></a></li>
         </ul>
 </div>
 <script type="text/javascript">
-    $(".wheel-button").wheelmenu();
+    $(".wheel-button").wheelmenu({
+        trigger: "hover",
+        animation: "fly",
+        angle: "NW"
+    });
 </script>
