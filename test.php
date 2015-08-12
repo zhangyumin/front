d64d6fc956e17f3efa0a4d2136b97a402d025303
zhangyumin <zhangyumin618@163.com>
2015-08-12 20:26:39 +0800

    fix bug

--- test.php
+++ test.php
@@ -10,17 +10,22 @@ and open the template in the editor.
     <body>
         <?php
         $configure_file=fopen("./tojbrowse/trackList.json", "r+");
-           $txt=file('./tojbrowse/trackList.json');
-           if(strlen($txt[count($txt)-2])==5)
+           $txt=file('../jbrowse/data/arab20150812035603/trackList.json');
+           echo strlen($txt[count($txt)-2]);
+           if(strlen($txt[count($txt)-2])==5){
+                echo "short";
                     fseek($configure_file, -9, SEEK_END);
-            else if(strlen($txt[count($txt)-2])==23)
+           }
+            else if(strlen($txt[count($txt)-2])==23){
+                echo "long";
                     fseek($configure_file,-33,SEEK_END);
-            fwrite($configure_file,",\n"
-                . "\t\"onClick\" : {\n"
-                . "\t\t\"url\" : \"../front/sequence.php?chr={seq_id}&gene={start}&strand={strand}\",\n"
-                . "\t\t\"label\" : \"see polyA site\",\n"
-                . "\t\t\"action\" : \"newwindow\"\n"
-                . "\t}}]}\n");
+            }
+//            fwrite($configure_file,",\n"
+//                . "\t\"onClick\" : {\n"
+//                . "\t\t\"url\" : \"../front/sequence.php?chr={seq_id}&gene={start}&strand={strand}\",\n"
+//                . "\t\t\"label\" : \"see polyA site\",\n"
+//                . "\t\t\"action\" : \"newwindow\"\n"
+//                . "\t}}]}\n");
 //        fwrite($configure_file, "heheheheheheheheeh!!!!");
         ?>
     </body>
