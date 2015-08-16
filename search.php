37043ceea3adcb949bc396d9e99b99342bcef336
zhangyumin <zhangyumin618@163.com>
2015-08-16 20:35:47 +0800

    add wheel menu , navbar, footer.

--- search.php
+++ search.php
@@ -1,1 +1,18 @@
-
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <title>Search</title>
+        <script src="./src/jquery-2.0.0.min.js"></script>
+    </head>
+    <body>
+        <?php
+            include"navbar.php";
+        ?>
+        <?php
+            include"wheelmenu.php";
+            ?>
+        <?php
+            include"footer.php";
+            ?>
+    </body>
+</html>
