4ff193dee15aa12848bc826d6f240e838d84d100
zhangyumin <zhangyumin618@163.com>
2015-08-18 15:20:19 +0800

    add task id

--- show_result.php
+++ show_result.php
@@ -150,7 +150,8 @@
                     $internal_priming_reads=$array_internal[1];
                     $pat=$array_pat[0];
                     $pac=$array_pac[0];
-
+                    
+                    echo "<span style=\"color:red\">Task id :".$_SESSION['file']."</span><br>";
                     echo "Input reads : $input_reads";
                     echo "<br>Low quality reads : $low_quality_reads";
                     echo "<br>Reads with tail : $reads_with_tail";
