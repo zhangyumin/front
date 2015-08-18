b3f98cd0da1cedb00bca7842afcaa78b6dc0dc33
zhangyumin <zhangyumin618@163.com>
2015-08-18 21:40:06 +0800

    add function of getting results back

--- search.php
+++ search.php
@@ -3,26 +3,45 @@
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
         <title>Search</title>
         <script src="./src/jquery-2.0.0.min.js"></script>
+        <style>
+            fieldset{
+                border-color: #5499c9 !important;
+                border-style: solid !important;
+                border-width: 2px !important;
+                padding: 5px 10px !important;
+            }   
+        </style>
     </head>
     <body>
         <?php
             include"navbar.php";
         ?>
-        <div class="search">
-            <div class="search_block">
-                
-            </div>
-            <div class="jump_block">
-                
-            </div>
-        </div>
-        <br>
-        <div class="getback">
-            <form method="post" id="getback" action="#">
-                <input name="getback"/>
-                <button type="submit">submit</button>
-            </form>
-        </div>
+       <br>
+        <fieldset style="margin: 0 auto;width: 70%;text-align: center;">
+                    <legend style="text-align:left;">
+                        <span class="h3_italic">
+                            <font color="#224055"><b>Search task</b>:Get your results back</font>
+                        </span>
+                    </legend>
+                    <form method="post" id="getback" action="#">
+                        Task ID:&nbsp;&nbsp;
+                        <input name="getback" style="width: 40%;"/>
+                        <button type="submit">submit</button>
+                    </form>
+        </fieldset>
+       <br>
+       <fieldset style="margin: 0 auto;width: 70%;">
+                    <legend>
+                        <span class="h3_italic" >
+                            <font color="#224055" ><b>Search</b>:Search and view the system samples</font>
+                        </span>
+                    </legend>
+                    <form method="post" id="getback" action="#">
+                        Task ID:&nbsp;&nbsp;
+                        <input name="getback" style="width: 40%;"/>
+                        <button type="submit">submit</button>
+                    </form>
+        </fieldset>
         <?php
             session_start();
             if($_POST['getback']!=NULL){
