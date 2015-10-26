65cfbe9b96e206378905dc8d862c12ac78e21da8
workstation <workstation@pc>
2015-10-26 22:35:36 +0800

    fix pac jtable bug

--- system_PAClist.php
+++ system_PAClist.php
@@ -1,22 +1,22 @@
 <?php
 try {
     $con=  mysql_connect("localhost","root","root");
-    mysql_select_db("db_bio",$con);
+    mysql_select_db("db_server",$con);
     if(empty ($_POST['search']))
     {
-        $result=  mysql_query("SELECT COUNT(*) AS RecordCount FROM pac_arab10;");
+        $result=  mysql_query("SELECT COUNT(*) AS RecordCount FROM t_".$_GET['species']."_pac;");
         $row=  mysql_fetch_array($result);
         $recordCount=$row['RecordCount'];
 
-        $result=  mysql_query("SELECT * FROM pac_arab10 ORDER BY ".$_GET['jtSorting']." LIMIT ".$_GET['jtStartIndex'].",".$_GET['jtPageSize'].";");
+        $result=  mysql_query("SELECT * FROM t_".$_GET['species']."_pac ORDER BY ".$_GET['jtSorting']." LIMIT ".$_GET['jtStartIndex'].",".$_GET['jtPageSize'].";");
     }
     else{
         $search=$_POST['search'];
-        $result=  mysql_query("SELECT COUNT(*) AS RecordCount FROM pac_arab10 WHERE gene LIKE '%".$search."%';");
+        $result=  mysql_query("SELECT COUNT(*) AS RecordCount FROM t_".$_GET['species']."_pac WHERE gene LIKE '%".$search."%';");
         $row=  mysql_fetch_array($result);
         $recordCount=$row['RecordCount'];
         
-        $result=  mysql_query("SELECT * FROM pac_arab10	 WHERE gene LIKE '%".$search."%' ORDER BY ".$_GET['jtSorting']." LIMIT ".$_GET['jtStartIndex'].",".$_GET['jtPageSize'].";");
+        $result=  mysql_query("SELECT * FROM t_".$_GET['species']."_pac WHERE gene LIKE '%".$search."%' ORDER BY ".$_GET['jtSorting']." LIMIT ".$_GET['jtStartIndex'].",".$_GET['jtPageSize'].";");
     }
     $rows=array();
     while($row=  mysql_fetch_array($result))
