c19d55a4c293124e7a7c0075da5ecaca52e2eabd
wxd <wxd@ubuntu.(none)>
2015-10-27 22:56:36 -0700

    update index.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
+
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
@@ -11,198 +12,137 @@ and open the template in the editor.
         <link href="./src/index.css" rel="stylesheet" type="text/css" />
         <script src="./src/jquery-2.0.0.min.js"></script>
         <script src="./src/jquery.slides.min.js"></script>
+        <!-- Mobile viewport optimisation -->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="stylesheet" type="text/css" href="js/wowslider/style.css" />
+        <script type="text/javascript" src="js/wowslider/jquery.js"></script>
+
+        <link href="./css/flexible-grids.css" rel="stylesheet" type="text/css"/>
+        <!--[if lte IE 7]>
+        <link href="./css/iehacks.min.css" rel="stylesheet" type="text/css" />
+        <![endif]-->
+
+        <!--[if lt IE 9]>
+        <script src="./js/html5shiv/html5shiv.js"></script>
+        <![endif]-->
+
         <!--<link rel="stylesheet" href="./src/font-awesome.min.css">-->
         <!--<link rel="stylesheet" href="./src/example.css">-->
-        <script>
-            $(function() {
-                    $(".page .tagbox ul li").hover(
-                            function(){
-                                $(this).find(".a_text").stop().delay(50).animate({"top":0,opacity:0.9},300)
-                            },
-                            function(){
-                                $(this).find(".a_text").stop().animate({"top":-200,opacity:0},300)
-                      });
-            });
-        </script>
-        <script>
-            $(function() {
-                $('#slides').slidesjs({
-                  width: 940,
-                  height: 528,
-                  navigation: false,
-                  play: {
-                    active: true,
-                    auto: true,
-                    interval: 4000,
-                    swap: true
-                  }
-                });
-              });
-        </script>
-             <style>
-    #slides {
-      display: none
-    }
-
-    #slides .slidesjs-navigation {
-      margin-top:5px;
-    }
-
-    a.slidesjs-next,
-    a.slidesjs-previous,
-    a.slidesjs-play,
-    a.slidesjs-stop {
-      background-image: url(pic/btns-next-prev.png);
-      background-repeat: no-repeat;
-      display:block;
-      width:12px;
-      height:18px;
-      overflow: hidden;
-      text-indent: -9999px;
-      float: left;
-      margin-right:5px;
-    }
-
-    a.slidesjs-next {
-      margin-right:10px;
-      background-position: -12px 0;
-    }
-
-    a:hover.slidesjs-next {
-      background-position: -12px -18px;
-    }
-
-    a.slidesjs-previous {
-      background-position: 0 0;
-    }
-
-    a:hover.slidesjs-previous {
-      background-position: 0 -18px;
-    }
-
-    a.slidesjs-play {
-      width:15px;
-      background-position: -25px 0;
-    }
-
-    a:hover.slidesjs-play {
-      background-position: -25px -18px;
-    }
-
-    a.slidesjs-stop {
-      width:18px;
-      background-position: -41px 0;
-    }
-
-    a:hover.slidesjs-stop {
-      background-position: -41px -18px;
-    }
-
-    .slidesjs-pagination {
-      margin: 7px 0 0;
-      float: right;
-      list-style: none;
-    }
-
-    .slidesjs-pagination li {
-      float: left;
-      margin: 0 1px;
-    }
-
-    .slidesjs-pagination li a {
-      display: block;
-      width: 13px;
-      height: 0;
-      padding-top: 13px;
-      background-image: url(pic/pagination.png);
-      background-position: 0 0;
-      float: left;
-      overflow: hidden;
-    }
-
-    .slidesjs-pagination li a.active,
-    .slidesjs-pagination li a:hover.active {
-      background-position: 0 -13px
-    }
-
-    .slidesjs-pagination li a:hover {
-      background-position: 0 -26px
-    }
-
-    #slides a:link,
-    #slides a:visited {
-      color: #333
-    }
-
-    #slides a:hover,
-    #slides a:active {
-      color: #9e2020
-    }
-
-    .navbar {
-      overflow: hidden
-    }
-  </style>
-  <style>
-    #slides {
-      display: none;
-    }
-
-    .slidepic {
-      margin: 0 auto;
-      width: 85%;
-    }
-
-    /* For tablets & smart phones */
-    @media (max-width: 767px) {
-      body {
-        padding-left: 20px;
-        padding-right: 20px;
-      }
-      .container {
-        width: auto
-      }
-    }
-
-    /* For smartphones */
-    @media (max-width: 480px) {
-      .container {
-        width: auto
-      }
-    }
-
-    /* For smaller displays like laptops */
-    @media (min-width: 768px) and (max-width: 979px) {
-      .container {
-        width: 724px
-      }
-    }
-
-    /* For larger displays */
-    @media (min-width: 1200px) {
-      .container {
-        width: 1170px
-      }
-    }
-  </style>
+       
     </head>
     <body>
-      fsdfasdfaf
-        <div style="width: 100%;margin:0 auto;" class="page">
-            <?php include './navbar.php'; ?>
-            <div class="slidepic">
-                <div id="slides">
-                    <img src="./pic/slide1.jpg" alt="Photo by: Missy S Link: http://www.flickr.com/photos/listenmissy/5087404401/">
-                    <img src="./pic/slide2.jpg" alt="Photo by: Daniel Parks Link: http://www.flickr.com/photos/parksdh/5227623068/">
-                    <img src="./pic/slide3.jpg" alt="Photo by: Mike Ranweiler Link: http://www.flickr.com/photos/27874907@N04/4833059991/">
-                  <a href="#" class="slidesjs-previous slidesjs-navigation"><i class="icon-chevron-left icon-large"></i></a>
-                  <a href="#" class="slidesjs-next slidesjs-navigation"><i class="icon-chevron-right icon-large"></i></a>
+
+        
+        <?php include './navbar.php'; ?>
+        <div class="ym-wrapper">
+          <div class="ym-wbox">
+            <div class="box info">
+              <div class="ym-grid linearize-level-1">
+                <div class="ym-g50 ym-gl">
+                  <h1>PolyA Browser</h1>
                 </div>
+                <div class="ym-g50 ym-gr">
+                  <div id="wowslider-container1">
+                    <div class="ws_images"><ul>
+                      <li><img src="pic/sliderimg/images/slide1.jpg" alt="slide1" title="slide1" id="wows1_0"/></li>
+                      <li><img src="pic/sliderimg/images/slide2.jpg" alt="Desert" title="slide2" id="wows1_1"/></li>
+                    </ul></div>
+                    <div class="ws_bullets">
+                      <div>
+                      <a href="#" title="326911"><span><img src="data1/tooltips/326911.jpg" alt="326911"/>1</span></a>
+                      <a href="#" title="Desert"><span><img src="data1/tooltips/desert.jpg" alt="Desert"/>2</span></a>
+                      </div>
+                    </div>
+                    <div class="ws_shadow"></div>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div style="height: 600px;border:1px solid #0066B3;text-align: center;" id="logo">这里是网站的介绍</div>
+
+            <div class="box info" id="introduction">
+              <h2>Introduction</h2>
+            </div>
+          </div>
         </div>
         <?php
             include"./footer.php";
         ?>
+        <script type="text/javascript" src="js/wowslider/wowslider.js"></script>
+        <script type="text/javascript" src="js/wowslider/script.js"></script>
+    </body>
+</html>
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <title>polyA browser</title>
+        <link href="./src/index.css" rel="stylesheet" type="text/css" />
+        <script src="./src/jquery-2.0.0.min.js"></script>
+        <script src="./src/jquery.slides.min.js"></script>
+        <!-- Mobile viewport optimisation -->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="stylesheet" type="text/css" href="js/wowslider/style.css" />
+        <script type="text/javascript" src="js/wowslider/jquery.js"></script>
+
+        <link href="./css/flexible-grids.css" rel="stylesheet" type="text/css"/>
+        <!--[if lte IE 7]>
+        <link href="./css/iehacks.min.css" rel="stylesheet" type="text/css" />
+        <![endif]-->
+
+        <!--[if lt IE 9]>
+        <script src="./js/html5shiv/html5shiv.js"></script>
+        <![endif]-->
+
+        <!--<link rel="stylesheet" href="./src/font-awesome.min.css">-->
+        <!--<link rel="stylesheet" href="./src/example.css">-->
+       
+    </head>
+    <body>
+
+        
+        <?php include './navbar.php'; ?>
+        <div class="ym-wrapper">
+          <div class="ym-wbox">
+            <div class="box info">
+              <div class="ym-grid linearize-level-1">
+                <div class="ym-g50 ym-gl">
+                  <h1>PolyA Browser</h1>
+                </div>
+                <div class="ym-g50 ym-gr">
+                  <div id="wowslider-container1">
+                    <div class="ws_images"><ul>
+                      <li><img src="pic/sliderimg/images/slide1.jpg" alt="slide1" title="slide1" id="wows1_0"/></li>
+                      <li><img src="pic/sliderimg/images/slide2.jpg" alt="Desert" title="slide2" id="wows1_1"/></li>
+                    </ul></div>
+                    <div class="ws_bullets">
+                      <div>
+                      <a href="#" title="326911"><span><img src="data1/tooltips/326911.jpg" alt="326911"/>1</span></a>
+                      <a href="#" title="Desert"><span><img src="data1/tooltips/desert.jpg" alt="Desert"/>2</span></a>
+                      </div>
+                    </div>
+                    <div class="ws_shadow"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="box info" id="introduction">
+              <h2>Introduction</h2>
+            </div>
+          </div>
+        </div>
+        <?php
+            include"./footer.php";
+        ?>
+        <script type="text/javascript" src="js/wowslider/wowslider.js"></script>
+        <script type="text/javascript" src="js/wowslider/script.js"></script>
     </body>
 </html>
