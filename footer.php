a563e6fd4da270795b7db78b305cc6a52f844ee5
wxd <wxd@ubuntu.(none)>
2015-10-27 23:14:01 -0700

    update footer.php

--- footer.php
+++ footer.php
@@ -1,5 +1,12 @@
 <div class="copy-right" style="font-family: 'Times New Roman','Times','serif';font-size: 15px;background-color: #5499C9;color: #fff;">
-    <center>© Copyright BMI Lab 2005-2013<script type="text/javascript" src="http://Register.xmu.edu.cn/RecordJS.aspx?RecordID=U501011"></script></center>
+    
+   <center>© Copyright BMI Lab 2005-2013<!-- <script type="text/javascript" src="http://Register.xmu.edu.cn/RecordJS.aspx?RecordID=U501011"></script> --></center>
+   <center>
+   		<a target="_blank" href="http://Register.xmu.edu.cn/Record.aspx?RecordID=U501011">
+			<img border="0" src="http://Register.xmu.edu.cn/Images/register.gif">
+		</a>
+		<a target="_blank" href="http://Register.xmu.edu.cn/Record.aspx?RecordID=U501011">U501011</a>
+   </center>
 </div>
 <div class="address"style="font-family: 'Times New Roman','Times','serif';font-size: 15px;background-color: #5499C9;color:#fff;">
     <center>Xiamen University, Xiamen 361005 China. e-mail:xhuister@xmu.edu.cn</center>
