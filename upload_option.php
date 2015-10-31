7b742a0ebc4053c184fe3b4932e4cc9c24339ec1
zym-workstation <zhangyumin618@163.com>
2015-10-31 21:42:49 +0800

    fix bugs

--- upload_option.php
+++ upload_option.php
@@ -35,7 +35,7 @@ and open the template in the editor.
         <!--<link rel="stylesheet" href="./src/example.css">-->
         
         <script type="text/template" id="qq-template">
-            <div class="qq-uploader-selector qq-uploader qq-gallery" qq-drop-area-text="Drop sequence files here">
+            <div class="qq-uploader-selector qq-uploader qq-gallery" qq-drop-area-text="Drop files here">
                 <div class="qq-total-progress-bar-container-selector qq-total-progress-bar-container">
                     <div role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" class="qq-total-progress-bar-selector qq-progress-bar qq-total-progress-bar"></div>
                 </div>
