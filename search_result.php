967ced775c7c34973449fc07a802921036f23b3f
wxd <wxd@ubuntu.(none)>
2015-11-04 01:32:04 -0800

    update search_result.php

--- search_result.php
+++ search_result.php
@@ -5,14 +5,14 @@
         <script src="./src/jquery-2.0.0.min.js"></script>
         <script src="./src/jquery.dataTables.min.js"type="text/javascript" ></script>
         <link href="./src/jquery.dataTables.css"type="text/css" rel="stylesheet"></link>
-        <style>
-             fieldset{
-                border-color: #5499c9 !important;
-                border-style: solid !important;
-                border-width: 2px !important;
-                padding: 5px 10px !important;
-            }
-        </style>
+        <link href="./css/flexible-grids.css" rel="stylesheet" type="text/css"/>
+        <!--[if lte IE 7]>
+        <link href="./css/iehacks.min.css" rel="stylesheet" type="text/css" />
+        <![endif]-->
+
+        <!--[if lt IE 9]>
+        <script src="./js/html5shiv/html5shiv.js"></script>
+        <![endif]-->
     </head>
     <body onload="set();getchr()">
          
@@ -211,124 +211,129 @@
                     }
                 }
         </script>
-        <fieldset style="margin: 10px auto 0 auto ;width: 95%;">
-                    <legend>
-                        <span class="h3_italic" >
-                            <font color="#224055" ><b>Search</b>:Search and view the system samples</font>
-                        </span>
-                    </legend>
-           <div style="width:60%;margin:0 auto;">
-               <form method="post" name="search" id="getback" action="#">
-                   <label for="species" style="margin-right:2%;">Species:</label>
-                   <select id="species" name="species" style="width:25%" onclick="getchr()">
-                        <option value="arab" selected="selected">Arabidopsis thaliana</option>
-                         <option value="japonica">Japonica rice</option>
-                        <option value="mtr">Medicago truncatula</option>
-                        <option value="chlamy">Chlamydomonas reinhardtii (Green alga)</option>
-                    </select>
-                   <label for="chr" style="margin: 0 1%">in</label>
-                        <select id="chr" name="chr" style="width:8%">
-                            <option value="all">All</option>
-                        </select>
-                   <label for="start" style="margin:0 1%;"> from</label>
-                        <input type="text" name="start" id="start" style="width:14%">
-                   <label for="end" style="margin:0 1%;"> to</label>
-                        <input type="text" name="end" id="end" style="width:14%"><br>
-<!--                        &nbsp;for
-                        <input type="text" name="keyword" style="width:20%;">-->
-                        <br><label for="gene_id">Gene ID:(use ',' to split different gene id)</label><br><textarea style="width:100%" name="gene_id" id="gene_id"></textarea><br>
-                        <br><label for="go_accession">Go term accession:(use ',' to split different gene id)</label><br><textarea style="width:100%" name='go_accession' id="go_accession"></textarea><br>
-                        <br><label for="go_name" style="padding-right:2%;">Go term name:</label><input type='text' name='go_name' id="go_name" style="width: 40%"/><br>
-                        <br><label for="function" style="padding-right:6.7%;">Function:</label><input type='text' name='function' id="function" style="width:40%;"/><br>
-                        <div style="text-align:center;">
-                            <button type="submit">submit</button>
-                            <button type="reset">reset</button>
-                            <input type="button" onclick="search_example()" value="Example">
-                            <script type="text/javascript">
-                                function search_example(){
-                                    if(document.getElementById("species").value=='arab'){
-                                        document.getElementById("go_accession").value="GO:0006888";
-                                        document.getElementById("start").value="10000";
-                                        document.getElementById("end").value="1000000";
-                                    }
-                                    else if(document.getElementById("species").value=='japonica'){
-                                        document.getElementById("go_accession").value="GO:0009987";
-                                        document.getElementById("start").value="10000";
-                                        document.getElementById("end").value="50000";
-                                    }
-                                    else if(document.getElementById("species").value=='mtr'){
-                                        document.getElementById("go_accession").value="GO:0003899";
-                                        document.getElementById("start").value="10000";
-                                        document.getElementById("end").value="1000000";
-                                    }
-                                    else if(document.getElementById("species").value=='chlamy'){
-                                        document.getElementById("go_accession").value="GO:0008131";
-                                        document.getElementById("start").value="10000";
-                                        document.getElementById("end").value="100000";
-                                    }
-                                }
-                            </script>
+        <div class="ym-wrapper">
+            <fieldset >
+                <legend>
+                    <h4>
+                        <font color="#224055" ><b>Search</b>:Search and view the system samples</font>
+                    </h4>
+                </legend>
+               <div class="box info ym-form">
+                   <form method="post" name="search" id="getback" action="#">
+                    <div class="ym-grid ym-fbox">
+                        <div class="ym-g33 ym-gl">
+                           <label for="species" style="margin-right:2%;">Species:</label>
+                           <select id="species" name="species" style="width:80%" onclick="getchr()">
+                                <option value="arab" selected="selected">Arabidopsis thaliana</option>
+                                 <option value="japonica">Japonica rice</option>
+                                <option value="mtr">Medicago truncatula</option>
+                                <option value="chlamy">Chlamydomonas reinhardtii (Green alga)</option>
+                            </select>
                         </div>
-                    </form>
-           </div>
-<!--           <div class="tips" style="font-size: 10px;">
-               Dataset description: detailed description on the searching dataset can be referred by unfolding more description in the dataset table.
-               <br>
-               Tips for searching: search using a ID, gene name, symbols or description variable in publications and databases.
-     
-           </div>-->
-        </fieldset>
-        <br><br>
-        <div id="table"  style="width: 90%;overflow-x: auto;background-color: #fff;margin:auto;">
-            <table id="result" class="display dataTable" cellspacing="0" role="grid" aria-describedby="example_infox" style="text-align: center;">
-            <thead>
-                <tr>
-                    <th>View</th>
-                    <th>Gene</th>
-                    <th>Chr</th>
-                    <th>ftr_start</th>
-                    <th>ftr_end</th>
-                    <th>Strand</th>
-                    <th>Ftr</th>
-                    <th>Gene Type</th>
-                    <!--<th>Pac</th>-->
-                    <th>Detail</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                            while($query_row=  mysql_fetch_row($query_result)){
-                                echo "<tr>";
-                                    echo "<td><a target=\"_blank\" href=\"../jbrowse/?data=data/arabidopsis&loc=$query_row[0]:$query_row[5]\"><span title=\"View the sequence in Jbrowse\" style=\"background-color:#0066cc;color:#FFFFFF;\">View</span></a></td>";
-                                    echo "<td>$query_row[8]</td>";
-                                    echo "<td>$query_row[0]</td>";
-                                    echo "<td>$query_row[5]</td>";
-                                    echo "<td>$query_row[6]</td>";
-                                    echo "<td>$query_row[1]</td>";
-                                    echo "<td>$query_row[4]</td>";
-                                    echo "<td>$query_row[9]</td>";
-                                    if($query_row[4]=='intergenic.igt'||$query_row[4]=='intergenic.pm'){
-                                        if($query_row[1]=='+')
-                                            echo "<td><a target=\"_blank\" href=\"./sequence_detail.php?species=".$_POST['species']."&seq=$query_row[8]&strand=1&flag=intergenic\"><span title=\"Get more information about this sequence\" style=\"background-color:#0066cc;color:#FFFFFF;\">Detail</span></a></td>";
-                                        else
-                                            echo "<td><a target=\"_blank\" href=\"./sequence_detail.php?species=".$_POST['species']."&seq=$query_row[8]&strand=-1&flag=intergenic\"><span title=\"Get more information about this sequence\" style=\"background-color:#0066cc;color:#FFFFFF;\">Detail</span></a></td>";
+                        <div class="ym-g33 ym-gl">
+                            <label for="chr" style="margin: 0 1%">in</label>
+                            <select id="chr" name="chr" style="width:80%">
+                                <option value="all">All</option>
+                            </select>
+                        </div>
+                        <div class="ym-g33 ym-gl">
+                            <label for="start" style="margin:0 1%;"> from</label>
+                            <input type="text" name="start" id="start" style="width:40%">
+                            <label for="end" style="margin:0 1%;"> to</label>
+                            <input type="text" name="end" id="end" style="width:40%"><br>
+                        </div>
+                    </div>
+                    <div class="ym-grid ym-fbox">
+                            <br><label for="gene_id">Gene ID:(use ',' to split different gene id)</label><br><textarea style="width:100%" name="gene_id" id="gene_id"></textarea><br>
+                            <br><label for="go_accession">Go term accession:(use ',' to split different gene id)</label><br><textarea style="width:100%" name='go_accession' id="go_accession"></textarea><br>
+                            <br><label for="go_name" >Go term name:</label><input type='text' name='go_name' id="go_name" class="ym-gr" style="width:89%;"/><br>
+                            <br><label for="function" >Function:</label><input type='text' name='function' id="function" class="ym-gr" style="width:89%;"/><br>
+                    </div>
+                    <div class="ym-grid ym-fbox">
+                                <button type="submit">submit</button>
+                                <button type="reset">reset</button>
+                                <input type="button" onclick="search_example()" value="Example">
+                    </div>
+                                <script type="text/javascript">
+                                    function search_example(){
+                                        if(document.getElementById("species").value=='arab'){
+                                            document.getElementById("go_accession").value="GO:0006888";
+                                            document.getElementById("start").value="10000";
+                                            document.getElementById("end").value="1000000";
+                                        }
+                                        else if(document.getElementById("species").value=='japonica'){
+                                            document.getElementById("go_accession").value="GO:0009987";
+                                            document.getElementById("start").value="10000";
+                                            document.getElementById("end").value="50000";
+                                        }
+                                        else if(document.getElementById("species").value=='mtr'){
+                                            document.getElementById("go_accession").value="GO:0003899";
+                                            document.getElementById("start").value="10000";
+                                            document.getElementById("end").value="1000000";
+                                        }
+                                        else if(document.getElementById("species").value=='chlamy'){
+                                            document.getElementById("go_accession").value="GO:0008131";
+                                            document.getElementById("start").value="10000";
+                                            document.getElementById("end").value="100000";
+                                        }
                                     }
-                                    else
-                                        echo "<td><a target=\"_blank\" href=\"./sequence_detail.php?species=".$_POST['species']."&seq=$query_row[8]\"><span title=\"Get more information about this sequence\" style=\"background-color:#0066cc;color:#FFFFFF;\">Detail</span></a></td>";
-                                echo "</tr>";
-                            }
-                ?>
-            </tbody>
-            </table>
-        </div>
-        <script>
-            $(document).ready(function(){
-                $('#result').dataTable({
-                    "lengthMenu":[[10,25,50,-1],[10,25,50,"all"]],
-                    "pagingType":"full_numbers"
+                                </script>
+                            </div>
+                        </form>
+               </div>
+            </fieldset>
+            <br><br>
+            <div id="table"  style="width: 90%;overflow-x: auto;background-color: #fff;margin:auto;">
+                <table id="result" class="display dataTable" cellspacing="0" role="grid" aria-describedby="example_infox" style="text-align: center;">
+                <thead>
+                    <tr>
+                        <th>View</th>
+                        <th>Gene</th>
+                        <th>Chr</th>
+                        <th>ftr_start</th>
+                        <th>ftr_end</th>
+                        <th>Strand</th>
+                        <th>Ftr</th>
+                        <th>Gene Type</th>
+                        <!--<th>Pac</th>-->
+                        <th>Detail</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                                while($query_row=  mysql_fetch_row($query_result)){
+                                    echo "<tr>";
+                                        echo "<td><a target=\"_blank\" href=\"../jbrowse/?data=data/arabidopsis&loc=$query_row[0]:$query_row[5]\"><span title=\"View the sequence in Jbrowse\" style=\"background-color:#0066cc;color:#FFFFFF;\">View</span></a></td>";
+                                        echo "<td>$query_row[8]</td>";
+                                        echo "<td>$query_row[0]</td>";
+                                        echo "<td>$query_row[5]</td>";
+                                        echo "<td>$query_row[6]</td>";
+                                        echo "<td>$query_row[1]</td>";
+                                        echo "<td>$query_row[4]</td>";
+                                        echo "<td>$query_row[9]</td>";
+                                        if($query_row[4]=='intergenic.igt'||$query_row[4]=='intergenic.pm'){
+                                            if($query_row[1]=='+')
+                                                echo "<td><a target=\"_blank\" href=\"./sequence_detail.php?species=".$_POST['species']."&seq=$query_row[8]&strand=1&flag=intergenic\"><span title=\"Get more information about this sequence\" style=\"background-color:#0066cc;color:#FFFFFF;\">Detail</span></a></td>";
+                                            else
+                                                echo "<td><a target=\"_blank\" href=\"./sequence_detail.php?species=".$_POST['species']."&seq=$query_row[8]&strand=-1&flag=intergenic\"><span title=\"Get more information about this sequence\" style=\"background-color:#0066cc;color:#FFFFFF;\">Detail</span></a></td>";
+                                        }
+                                        else
+                                            echo "<td><a target=\"_blank\" href=\"./sequence_detail.php?species=".$_POST['species']."&seq=$query_row[8]\"><span title=\"Get more information about this sequence\" style=\"background-color:#0066cc;color:#FFFFFF;\">Detail</span></a></td>";
+                                    echo "</tr>";
+                                }
+                    ?>
+                </tbody>
+                </table>
+            </div>
+            <script>
+                $(document).ready(function(){
+                    $('#result').dataTable({
+                        "lengthMenu":[[10,25,50,-1],[10,25,50,"all"]],
+                        "pagingType":"full_numbers"
+                    });
                 });
-            });
-        </script>
+            </script>
+        </div>
         <div class="bottom">
         <?php
             include"footer.php";
