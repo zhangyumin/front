ec164d48a9487daa400068e9668f70000bc96395
zym-workstation <zhangyumin618@163.com>
2015-11-07 19:38:40 +0800

    get sql result and output fa file

--- test2.php
+++ test2.php
@@ -1,5 +1,13 @@
 <?php
-//    var_dump($_POST);
-    print_r(json_encode($_POST));
-//    echo "haha";
+    $c=array();
+    $con=  mysql_connect("localhost","root","root");
+    mysql_select_db("db_server",$con);
+    $a=mysql_query("select gene from db_server.t_mtr_gff_genes;");
+    while($b=mysql_fetch_row($a)){
+        array_push($c, $b[0]);
+    }
+    foreach ($c as $key => $value) {
+        echo $value."\n";
+        shell_exec("./src/perl/SVR_extractSeqbyID.pl $value < /var/www/front/seq/mtr/t_mtr_gff_genes.fa > /var/www/front/tojbrowse/mtr/$value.fa 2>&1");
+    }
 ?>
