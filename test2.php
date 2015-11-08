49b06036851bd48addceb36eb0030d279ca8dfdf
zym-workstation <zhangyumin618@163.com>
2015-11-08 22:04:28 +0800

    test fa file

--- test2.php
+++ test2.php
@@ -1,13 +1,4 @@
 <?php
-    $c=array();
-    $con=  mysql_connect("localhost","root","root");
-    mysql_select_db("db_server",$con);
-    $a=mysql_query("select gene from db_server.t_mtr_gff_genes;");
-    while($b=mysql_fetch_row($a)){
-        array_push($c, $b[0]);
-    }
-    foreach ($c as $key => $value) {
-        echo $value."\n";
-        shell_exec("./src/perl/SVR_extractSeqbyID.pl $value < /var/www/front/seq/mtr/t_mtr_gff_genes.fa > /var/www/front/tojbrowse/mtr/$value.fa 2>&1");
-    }
+    $a=  file_get_contents("./seq/arab/".strtoupper("at1g01020").".fa");
+    echo $a;
 ?>
