ab1121d54b74a1de88cb789e16472c82e41e4828
zym-workstation <zhangyumin618@163.com>
2015-11-12 20:36:54 +0800

    fix bugs

--- Analysis_PAClist.php
+++ Analysis_PAClist.php
@@ -14,11 +14,11 @@ try {
     }
     else{
         $search=$_POST['search'];
-        $result=  mysql_query("SELECT COUNT(*) AS RecordCount FROM Analysis_".$_SESSION['file']." WHERE gene LIKE '%".$search."%' or coord LIKE '%".$search."%' or chr LIKE '%".$search."%' or ftr_start LIKE '%".$search."%' or ftr_end LIKE '%".$search."%' or ftr LIKE '%".$search."%' or gene_type LIKE '%".$search."%';");
+        $result=  mysql_query("SELECT COUNT(*) AS RecordCount FROM Analysis_".$_SESSION['file']." WHERE gene LIKE '%".$search."%' or gene_type LIKE '%".$search."%';");
         $row=  mysql_fetch_array($result);
         $recordCount=$row['RecordCount'];
         
-        $result=  mysql_query("SELECT * FROM Analysis_".$_SESSION['file']." WHERE gene LIKE '%".$search."%' or coord LIKE '%".$search."%' or chr LIKE '%".$search."%' or ftr_start LIKE '%".$search."%' or ftr_end LIKE '%".$search."%' or ftr LIKE '%".$search."%' or gene_type LIKE '%".$search."%' ORDER BY ".$_GET['jtSorting']." LIMIT ".$_GET['jtStartIndex'].",".$_GET['jtPageSize'].";");
+        $result=  mysql_query("SELECT * FROM Analysis_".$_SESSION['file']." WHERE gene LIKE '%".$search."%' or gene_type LIKE '%".$search."%' ORDER BY ".$_GET['jtSorting']." LIMIT ".$_GET['jtStartIndex'].",".$_GET['jtPageSize'].";");
     }
     $rows=array();
     while($row=  mysql_fetch_array($result))
