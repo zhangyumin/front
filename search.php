48ae942b5c7f22ac6d801798bee131ca01a87cca
zym-workstation <zhangyumin618@163.com>
2015-11-16 21:50:57 +0800

    fix bugs

--- search.php
+++ search.php
@@ -26,7 +26,7 @@
             mysql_select_db("db_server",$con);
             include"navbar.php";
         ?>
-
+        hahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahaha
         <script type="text/javascript">
 
         $(document).ready(function(){
