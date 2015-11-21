e87182634078156f57db7b2869ba7cfc8c005a2f
zhangyumin618@163.com <zhangyumin618@163.com>
2015-11-21 15:54:11 +0800

    change session['file'] to session['search']

--- Search_PAClist.php
+++ Search_PAClist.php
@@ -6,19 +6,19 @@ try {
     mysql_select_db("db_user",$con);
     if(empty ($_POST['search']))
     {
-        $result=  mysql_query("SELECT COUNT(*) AS RecordCount FROM Search_".$_SESSION['file'].";");
+        $result=  mysql_query("SELECT COUNT(*) AS RecordCount FROM Search_".$_SESSION['search'].";");
         $row=  mysql_fetch_array($result);
         $recordCount=$row['RecordCount'];
 
-        $result=  mysql_query("SELECT * FROM Search_".$_SESSION['file']." ORDER BY ".$_GET['jtSorting']." LIMIT ".$_GET['jtStartIndex'].",".$_GET['jtPageSize'].";");
+        $result=  mysql_query("SELECT * FROM Search_".$_SESSION['search']." ORDER BY ".$_GET['jtSorting']." LIMIT ".$_GET['jtStartIndex'].",".$_GET['jtPageSize'].";");
     }
     else{
         $search=$_POST['search'];
-        $result=  mysql_query("SELECT COUNT(*) AS RecordCount FROM Search_".$_SESSION['file']." WHERE gene LIKE '%".$search."%' or coord LIKE '%".$search."%' or chr LIKE '%".$search."%' or ftr_start LIKE '%".$search."%' or ftr_end LIKE '%".$search."%' or ftr LIKE '%".$search."%' or gene_type LIKE '%".$search."%';");
+        $result=  mysql_query("SELECT COUNT(*) AS RecordCount FROM Search_".$_SESSION['search']." WHERE gene LIKE '%".$search."%' or coord LIKE '%".$search."%' or chr LIKE '%".$search."%' or ftr_start LIKE '%".$search."%' or ftr_end LIKE '%".$search."%' or ftr LIKE '%".$search."%' or gene_type LIKE '%".$search."%';");
         $row=  mysql_fetch_array($result);
         $recordCount=$row['RecordCount'];
         
-        $result=  mysql_query("SELECT * FROM Search_".$_SESSION['file']." WHERE gene LIKE '%".$search."%' or coord LIKE '%".$search."%' or chr LIKE '%".$search."%' or ftr_start LIKE '%".$search."%' or ftr_end LIKE '%".$search."%' or ftr LIKE '%".$search."%' or gene_type LIKE '%".$search."%' ORDER BY ".$_GET['jtSorting']." LIMIT ".$_GET['jtStartIndex'].",".$_GET['jtPageSize'].";");
+        $result=  mysql_query("SELECT * FROM Search_".$_SESSION['search']." WHERE gene LIKE '%".$search."%' or coord LIKE '%".$search."%' or chr LIKE '%".$search."%' or ftr_start LIKE '%".$search."%' or ftr_end LIKE '%".$search."%' or ftr LIKE '%".$search."%' or gene_type LIKE '%".$search."%' ORDER BY ".$_GET['jtSorting']." LIMIT ".$_GET['jtStartIndex'].",".$_GET['jtPageSize'].";");
     }
     $rows=array();
     while($row=  mysql_fetch_array($result))
