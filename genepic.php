e8867de57d4b0fad29efd200e9b1676e16ab8999
zhangyumin618@163.com <zhangyumin618@163.com>
2015-12-12 10:18:51 +0800

    delete analysis sample

--- genepic.php
+++ genepic.php
@@ -48,10 +48,10 @@ and open the template in the editor.
             }
 //            var_dump($sample_selected);
             $sample=  mysql_query("select label from t_sample_desc where species='".$_GET['species']."';");
-                while ($sample_num=  mysql_fetch_row($sample)){
-                    $num++;
-                    array_push($samples,$sample_num[0]);
-                }
+            while ($sample_num=  mysql_fetch_row($sample)){
+                $num++;
+                array_push($samples,$sample_num[0]);
+            }
             //声明存储各个sample的loc,talbe,col和tagnum数组
             for($i=1;$i<=$num;$i++){
                 $pa_loc="pa_loc".$i;
