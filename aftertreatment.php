e0ee018c7215d61b31c24c20c7e867f8aad3b271
zhangyumin618@163.com <zhangyumin618@163.com>
2015-12-15 21:39:17 +0800

    fix bugs for sys_real

--- aftertreatment.php
+++ aftertreatment.php
@@ -108,6 +108,7 @@
                        $sysQry.=");";
                $query_result=mysql_query($sysQry);
              }
+//             file_put_contents("/var/www/front/searched/test.txt",$query_result);
     }
     //如果有$_session['file'],则上传处理过数据.
     else{
@@ -211,7 +212,7 @@
                $usr_query_result=mysql_query($usrQry);
              }
              $merge="./src/perl/PAT_mergePAC.pl -smptbls 'usrQryPAC_".$_SESSION['analysis'].";sysQryPAC_".$_SESSION['analysis']."' -reftbl usrQryPAC_".$_SESSION['analysis']." -smpcols '".implode(':',$_SESSION['file_real'] ).";".implode(':', $_SESSION['sys_real'])."' -smplbls '".implode(':',$_SESSION['file_real'] ).";".implode(':', $_SESSION['sys_real'])."' -otbl SearchedPAC_".$_SESSION['analysis']." -udist 24 -conf ./src/r/db_2.xml";
-//             echo $merge;
+//             file_put_contents("/var/www/front/searched/test.txt","1".$_SESSION['sys_real_arab']."2".$_SESSION['sys_real_chlamy']."3".$_SESSION['sys_real_japonica']."4".$_SESSION['sys_real_mtr']);
              shell_exec($merge);
         }
     if($_GET['method']=='degene'){
@@ -231,6 +232,7 @@
         $_SESSION['sample']=  array_merge($_POST['sample1'],$_POST['sample2']);
         $degene_cmd="Rscript /var/www/front/src/r/R_DEgene.r ofile='degene.".$_SESSION['analysis']."' method=$method adj=$Adj sig=$sig minpat=$minpat donorm=$donorm path='/var/www/front/searched/' intbl=SearchedPAC_".$_SESSION['analysis']." cols='".implode(':',$_POST['sample1'] ).";".implode(':', $_POST['sample2'])."' groups=column1:column2 conf=/var/www/front/src/r/db_2.xml";
         //$degene_cmd="Rscript /var/www/html/front/src/r/R_pairDEgene.r minrep=1 minpat=5 donorm=0 path='/home/zym/data/' intbl=PAC_sys_arab10 cols='oxt6_leaf_1:oxt6_leaf_2;wt_leaf_1:wt_leaf_2' groups=sys:user conf=/var/www/html/front/db.xml 2>&1";
+        file_put_contents("/var/www/front/searched/test.txt", $degene_cmd);
 //        echo $degene_cmd;
         if(count($_POST['sample1'])>=1&&count($_POST['sampe2']>=1))
         {
