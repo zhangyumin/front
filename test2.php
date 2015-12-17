d8b922260faff2a3273849c3fb902875acd1256e
zhangyumin618@163.com <zhangyumin618@163.com>
2015-12-17 21:57:32 +0800

    test push known key-array

--- test2.php
+++ test2.php
@@ -1,12 +1,7 @@
 <?php
-    $con=  mysql_connect("localhost","root","root");
-    mysql_select_db("db_server",$con);
-    mysql_query("update t_arab_pa2 set oxt6_root_med = oxt6_root_1 where oxt6_root_1 >= oxt6_root_2 and oxt6_root_1 <= oxt6_root_3");
-    mysql_query("update t_arab_pa2 set oxt6_root_med = oxt6_root_1 where oxt6_root_1 <= oxt6_root_2 and oxt6_root_1 >= oxt6_root_3");
-    
-    mysql_query("update t_arab_pa2 set oxt6_root_med = oxt6_root_2 where oxt6_root_2 >= oxt6_root_1 and oxt6_root_2 <= oxt6_root_3");
-    mysql_query("update t_arab_pa2 set oxt6_root_med = oxt6_root_2 where oxt6_root_2 <= oxt6_root_1 and oxt6_root_2 >= oxt6_root_3");
-    
-    mysql_query("update t_arab_pa2 set oxt6_root_med = oxt6_root_3 where oxt6_root_3 >= oxt6_root_2 and oxt6_root_3 <= oxt6_root_1");
-    mysql_query("update t_arab_pa2 set oxt6_root_med = oxt6_root_3 where oxt6_root_3 <= oxt6_root_2 and oxt6_root_3 >= oxt6_root_1");
+    $a = array();
+    for($i = 0; $i < 15; $i+=2){
+        $a[$i] = $i;
+    }
+    var_dump($a);
 ?>
