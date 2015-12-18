6cad1d88cda210939f6e7f57f3ee2a81292601c3
zhangyumin618@163.com <zhangyumin618@163.com>
2015-12-18 11:26:04 +0800

    test array add

--- test2.php
+++ test2.php
@@ -1,5 +1,21 @@
 <?php
-    $a = array('0'=>'2','1'=>'4','3'=>'8','a'=>'100');
-    $b = array('0'=>'5','2'=>'4','b'=>'33','a'=>'22');
-    var_dump($a+$b);
+    $a = array('0'=>'2','1'=>'4','2'=>'8','3'=>'100');
+    $b = array('0'=>'5','1'=>'4','2'=>'33','4'=>'22');
+    function array_add($a,$b){
+        //根据键名获取两个数组的交集
+        $arr=array_intersect_key($a, $b);
+        //遍历第二个数组，如果键名不存在与第一个数组，将数组元素增加到第一个数组
+        foreach($b as $key=>$value){
+            if(!array_key_exists($key, $a)){
+                $a[$key]=$value;
+            }
+        }
+            //计算键名相同的数组元素的和，并且替换原数组中相同键名所对应的元素值
+        foreach($arr as $key=>$value){
+            $a[$key]=$a[$key]+$b[$key];
+        }
+        //返回相加后的数组
+        return $a;
+    }
+    var_dump(array_add($a, $b));
 ?>
