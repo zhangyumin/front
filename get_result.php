4c37015117b3ca07af7a017e323a143098b1db7b
zhangyumin618@163.com <zhangyumin618@163.com>
2015-12-22 10:19:11 +0800

    complete add task ip

--- get_result.php
+++ get_result.php
@@ -8,6 +8,28 @@
             $_SESSION['species']=$_POST['species'];
             $filename=$_SESSION['file'].".fa";
             $filepath="./data/".$_SESSION['file']."/";
+            
+            #获得ip并存入数据库
+            function getIP() { 
+                if (@$_SERVER["HTTP_X_FORWARDED_FOR"]) 
+                    $ip = $_SERVER["HTTP_X_FORWARDED_FOR"]; 
+                else if (@$_SERVER["HTTP_CLIENT_IP"]) 
+                    $ip = $_SERVER["HTTP_CLIENT_IP"]; 
+                else if (@$_SERVER["REMOTE_ADDR"]) 
+                    $ip = $_SERVER["REMOTE_ADDR"]; 
+                else if (@getenv("HTTP_X_FORWARDED_FOR"))
+                    $ip = getenv("HTTP_X_FORWARDED_FOR"); 
+                else if (@getenv("HTTP_CLIENT_IP")) 
+                    $ip = getenv("HTTP_CLIENT_IP"); 
+                else if (@getenv("REMOTE_ADDR")) 
+                    $ip = getenv("REMOTE_ADDR"); 
+                else 
+                    $ip = "Unknown"; 
+                return $ip; 
+            }
+            $uip = getIP();
+            $mysqltime=date('Y-m-d H:i:s',time());
+            mysql_query("INSERT INTO `User_Task`(`id`, `ip`, `time`) VALUES ('".$_SESSION['file']."','$uip','$mysqltime')");
             //echo $filepath;
             #传递上传文件参数设置
 //                $_SESSION['qct']=$_POST['qct'];
