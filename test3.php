fd36f2005194bf06552a07c27db377a5b7a07291
zhangyumin618@163.com <zhangyumin618@163.com>
2015-12-27 02:50:12 +0800

    reconstitution of sequence_viewer

--- test3.php
+++ test3.php
@@ -1,16 +1,10 @@
-<style>
-    .q{
-        display: inline;
-        background-color: green;
-    }
-    .e{
-        display: inline;
-        background-color:yellow;
-    }
-    .w{
-        display: inline;
-        background-color: red;
-    }
-</style>
-<div class="q">ABCDEF<span class="w">GHIJKLMN</div><div class="e">OPQRSTU</span>VWXYZ</div><br><br>
-<span class="q">ABCDEF<span class="w">GHIJKLMN</span><span class="e">OPQRSTU</span>VWXYZ</span>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <title>Search</title>
+        <script src="./src/jquery-1.10.1.min.js"></script>
+    </head>
+    <body>
+        
+    </body>
+</html>
