f65f21e0f7e64bed7ad5612afce2dc3ffc28d9fe
zhangyumin618@163.com <zhangyumin618@163.com>
2015-12-29 10:05:28 +0800

    test span+div

--- test3.php
+++ test3.php
@@ -1,4 +1,16 @@
-<?php
-    session_start();
-    var_dump($_SESSION['sample']);
-?>
+<style>
+    .q{
+        display: inline;
+        background-color: green;
+    }
+    .e{
+        display: inline;
+        background-color:yellow;
+    }
+    .w{
+        display: inline;
+        background-color: red;
+    }
+</style>
+<div class="q">ABCDEF<span class="w">GHIJKLMN</div><div class="e">OPQRSTU</span>VWXYZ</div><br><br>
+<span class="q">ABCDEF<span class="w">GHIJKLMN</span><span class="e">OPQRSTU</span>VWXYZ</span>
