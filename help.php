9f435721e659012eb396c39262eb967a68d235eb
zhangyumin618@163.com <zhangyumin618@163.com>
2016-01-01 22:24:02 +0800

    new page for help

---
+++ help.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <title>polyA browser</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="./css/flexible-grids.css" rel="stylesheet" type="text/css"/>
+        <script src="./src/jquery-1.10.1.min.js"></script>
+    </head>
+    <body>
+        <?php include './navbar.php'; ?>
+        <?php
+            include"./footer.php";
+        ?>
+    </body>
+</html>
