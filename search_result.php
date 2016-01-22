32dd6ebbf97d19e56ecfcfbe09a39c14f8a06ada
zhangyumin618@163.com <zhangyumin618@163.com>
2016-01-22 21:22:51 +0800

    fix export sequence style

--- search_result.php
+++ search_result.php
@@ -290,7 +290,7 @@
         </div>
         <!--          浮层框架开始         -->
         <div id="Layer2" align="center" style="border: 1px solid;position: absolute; z-index: 3; left: 40%; top: 30%;background-color: #fff; display: none;" >
-            <table width="400" height="100" border="0" cellpadding="0" cellspacing="0" style="border: 0    solid    #e7e3e7;border-collapse: collapse ;" >
+            <table width="350" height="100" border="0" cellpadding="0" cellspacing="0" style="border: 0    solid    #e7e3e7;border-collapse: collapse ;" >
                 <tr>
                     <td style="background-color: #73A2d6; color: #fff; padding-left: 4px; padding-top: 2px;font-weight: bold; font-size: 12px;" height="10" valign="middle">
                          <div align="right">
@@ -302,16 +302,16 @@
                 <tr>
                     <td align="center">
                         <form name="pac_export" method="post" action="export_seq.php?source=Search&species=<?php echo $species; ?>" target="_blank">
-                            method<select id="method" name="method" onchange="ChgMtd()" style="margin-left:70;margin-bottom: 5">
+                            method<select id="method" name="method" onchange="ChgMtd()" style="margin-left:70;margin-bottom: 5;width: 226px">
                                 <option value="choose">Please choose</option>
                                 <option value="pacs">export sequences of PACs</option>
                                 <option value="pacs-region">export sequences of regions of  PACs</option>
                                 <option value="seq">export gene sequences</option>
                             </select><br>
                             <div id="pacs" style="display:none">
-                                upstream (nt) <input type="text" value="200" name='upstream' style="margin-left:35;margin-bottom: 5"></input><br>
-                                downstream (nt) <input type="text" value="200" name='downstream' style="margin-left:20;margin-bottom: 5"></input><br>
-                                PAC in region <select name='pac_region' style="margin-left:33;margin-bottom: 5">
+                                upstream (nt) <input type="text" value="200" name='upstream' style="margin-left:35;margin-bottom: 5;width: 226px"></input><br>
+                                downstream (nt) <input type="text" value="200" name='downstream' style="margin-left:20;margin-bottom: 5;width: 226px"></input><br>
+                                PAC in region <select name='pac_region' style="margin-left:33;margin-bottom: 5;width: 226px">
                                     <option value="all">all</option>
                                     <option value="genomic-region">genomic region</option>
                                     <option value="3TUR">3'UTR</option>
@@ -323,7 +323,7 @@
                                 </select>
                             </div>
                             <div id="pacs-region" style="display:none">
-                                region of PACs <select name='pacs_region' style="margin-left:26;margin-bottom: 5">
+                                region of PACs <select name='pacs_region' style="margin-left:26;margin-bottom: 5;width: 226px">
                                     <option value="all">all</option>
                                     <option value="genomic-region">genomic region</option>
                                     <option value="3TUR">3'UTR</option>
@@ -335,11 +335,11 @@
                                 </select>
                             </div>
                             <div id="seq" style="display:none">
-                                annotation version <select name='anno_version' style="margin-left:9;margin-bottom: 5">
+                                annotation version <select name='anno_version' style="margin-left:9;margin-bottom: 5;width: 226px">
                                     <option value="raw-annotation">raw annotation</option>
                                     <option value="3utr-extended-annotation">3' UTR extended annotation</option>
                                 </select><br>
-                                export <select name='export' style="margin-left:73;margin-bottom: 5">
+                                export <select name='export' style="margin-left:73;margin-bottom: 5;width: 226px">
                                     <option value="whole-gene">whole gene</option>
                                     <option value="joined-cds">joined CDS</option>
                                     <option value="3utr-only">3' UTR only</option>
