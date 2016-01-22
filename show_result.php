afefae5d1b6cfa1704ab6290c188fd3ce0ecacf7
zhangyumin618@163.com <zhangyumin618@163.com>
2016-01-22 21:28:24 +0800

    fix export sequence style

--- show_result.php
+++ show_result.php
@@ -266,15 +266,15 @@
             <tr>
                 <td align="center">
                     <form name="pac_export" method="post" action="export_seq.php?source=PAC&species=<?php echo $_SESSION['species']; ?>" target="_blank">
-                        method<select id="method" name="method" onchange="ChgMtd()" style="margin-left:70;margin-bottom: 5;width: 226px">
+                        Method<select id="method" name="method" onchange="ChgMtd()" style="margin-left:70;margin-bottom: 5;width: 226px">
                             <option value="choose">Please choose</option>
                             <option value="pacs">export sequences of PACs</option>
                             <option value="pacs-region">export sequences of regions of  PACs</option>
                             <option value="seq">export gene sequences</option>
                         </select><br>
                         <div id="pacs" style="display:none">
-                            upstream (nt) <input type="text" value="200" name='upstream' style="margin-left:35;margin-bottom: 5;width: 226px"></input><br>
-                            downstream (nt) <input type="text" value="200" name='downstream' style="margin-left:20;margin-bottom: 5;width: 226px"></input><br>
+                            Upstream (nt) <input type="text" value="200" name='upstream' style="margin-left:35;margin-bottom: 5;width: 226px"></input><br>
+                            Downstream (nt) <input type="text" value="200" name='downstream' style="margin-left:20;margin-bottom: 5;width: 226px"></input><br>
                             PAC in region <select name='pac_region' style="margin-left:33;margin-bottom: 5;width: 226px">
                                 <option value="all">all</option>
                                 <option value="genomic-region">genomic region</option>
@@ -287,7 +287,7 @@
                             </select>
                         </div>
                         <div id="pacs-region" style="display:none">
-                            region of PACs <select name='pacs_region' style="margin-left:26;margin-bottom: 5;width: 226px">
+                            Region of PACs <select name='pacs_region' style="margin-left:26;margin-bottom: 5;width: 226px">
                                 <option value="all">all</option>
                                 <option value="genomic-region">genomic region</option>
                                 <option value="3TUR">3'UTR</option>
@@ -299,11 +299,11 @@
                             </select>
                         </div>
                         <div id="seq" style="display:none">
-                            annotation version <select name='anno_version' style="margin-left:9;margin-bottom: 5;width: 226px">
+                            Annotation version <select name='anno_version' style="margin-left:9;margin-bottom: 5;width: 226px">
                                 <option value="raw-annotation">raw annotation</option>
                                 <option value="3utr-extended-annotation">3' UTR extended annotation</option>
                             </select><br>
-                            export <select name='export' style="margin-left:73;margin-bottom: 5;width: 226px">
+                            Export <select name='export' style="margin-left:73;margin-bottom: 5;width: 226px">
                                 <option value="whole-gene">whole gene</option>
                                 <option value="joined-cds">joined CDS</option>
                                 <option value="3utr-only">3' UTR only</option>
