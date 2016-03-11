8e0a2c82be7d66c956f00f33b3d1893b1c6c8fd6
zhangyumin <zhangyumin618@163.com>
2016-03-11 21:58:08 +0800

    fix footer

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<div class="copy-right" style="font-family: 'Times New Roman','Times','serif';font-size: 15px;background-color: #5499C9;color: #fff;margin-top:20px;">
+<div class="copy-right" style="font-family: 'Times New Roman','Times','serif';font-size: 15px;background-color: #5db95b;color: #fff;margin-top:20px;">
     
    <center>© Copyright BMI Lab 2005-2013<!-- <script type="text/javascript" src="http://Register.xmu.edu.cn/RecordJS.aspx?RecordID=U501011"></script> --></center>
    <center>
@@ -8,6 +8,6 @@
 		<a target="_blank" href="http://Register.xmu.edu.cn/Record.aspx?RecordID=U501011">U501011</a>
    </center>
 </div>
-<div class="address"style="font-family: 'Times New Roman','Times','serif';font-size: 15px;background-color: #5499C9;color:#fff;">
+<div class="address"style="font-family: 'Times New Roman','Times','serif';font-size: 15px;background-color: #5db95b;color:#fff;">
     <center>Xiamen University, Xiamen 361005 China. e-mail:xhuister@xmu.edu.cn</center>
 </div>
