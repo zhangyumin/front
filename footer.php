9ca107d94f049782cdb8951bf6fbc8aeaa6910fd
zhangyumin618@163.com <zhangyumin618@163.com>
2016-03-12 16:18:37 +0800

    change footer background-color

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<div class="copy-right" style="font-family: 'Times New Roman','Times','serif';font-size: 15px;background-color: #5db95b;color: #fff;margin-top:20px;">
+<div class="copy-right" style="font-family: 'Times New Roman','Times','serif';font-size: 15px;background-color: #eee;color: #5db95b;margin-top:20px;border-top: 1px solid rgba(0,0,0,0.2)">
     
    <center>© Copyright BMI Lab 2005-2013<!-- <script type="text/javascript" src="http://Register.xmu.edu.cn/RecordJS.aspx?RecordID=U501011"></script> --></center>
    <center>
@@ -8,6 +8,6 @@
 		<a target="_blank" href="http://Register.xmu.edu.cn/Record.aspx?RecordID=U501011">U501011</a>
    </center>
 </div>
-<div class="address"style="font-family: 'Times New Roman','Times','serif';font-size: 15px;background-color: #5db95b;color:#fff;">
+<div class="address"style="font-family: 'Times New Roman','Times','serif';font-size: 15px;background-color: #eee;color:#5db95b;">
     <center>Xiamen University, Xiamen 361005 China. e-mail:xhuister@xmu.edu.cn</center>
 </div>
