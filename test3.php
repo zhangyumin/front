5b1ef48e69821fc72da88ce67a0566d314a068b1
zhangyumin618@163.com <zhangyumin618@163.com>
2016-03-23 15:23:39 +0800

    test

--- test3.php
+++ test3.php
@@ -1,23 +1,3 @@
-<html>
-    <head>
-        <title>抓AD专用页面</title>
-    </head>
-    <body onload="GetAd()">
-        <div id="a"></div>
-        <script>
-            function GetAd(){
-                var omain = document.getElementById("oMain");
-                var a = document.getElementById("a");
-                if(omain){
-                    alert('find advertisement')
-                }
-                else{
-                    for(i = 0 ;i < 10000000000 ; i++){
-                        
-                    }
-                    location.reload();
-                }
-            }
-        </script>
-    </body>
-</html>
+<?php
+    var_dump($_POST);
+?>
