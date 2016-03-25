27fdd78fbb1d6f63b9266bd83b7168565d62791c
zhangyumin618@163.com <zhangyumin618@163.com>
2016-03-25 20:22:56 +0800

    jquery-webui-popover

--- test3.php
+++ test3.php
@@ -1,3 +1,19 @@
-<?php
-    var_dump($_POST);
-?>
+<html>
+    <head>
+        <script src="./src/jquery-1.10.1.min.js"></script>
+        <script src="./src/jquery.webui-popover.js"></script>
+        <link href="./src/jquery.webui-popover.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <a>hahaha</a>
+        <script>
+            $('a').webuiPopover({
+                placement:'right',//值: auto,top,right,bottom,left,top-right,top-left,bottom-right,bottom-left
+                title:'Title',
+                content:'Content<a href="www.baidu.com">click</a>',
+                trigger:'hover',
+                type:'html'
+            });
+        </script>
+    </body>
+</html>
