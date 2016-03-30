1a7cb7f132c1d1096f998eeeca09bf678cb029de
zhangyumin618@163.com <zhangyumin618@163.com>
2016-03-30 10:09:39 +0800

    add href to help page

--- sequence_detail.php
+++ sequence_detail.php
@@ -1101,8 +1101,8 @@
                             }
                              $('#tip').webuiPopover({
                                 placement:'right',//值: auto,top,right,bottom,left,top-right,top-left,bottom-right,bottom-left
-                                title:'Gene modal color',
-                                content:'&nbsp;&nbsp;<span style="text-align:center;background-color: #878787">&nbsp;&nbsp;&nbsp;&nbsp;</span>Extended 3\'UTR<br><br>&nbsp;&nbsp;<span style="text-align:center;background-color:#9FE0F6">&nbsp;&nbsp;&nbsp;&nbsp;</span>AMB',//<br><br>&nbsp;&nbsp;<span style="text-align:center;background-color:#00ABD8">&nbsp;&nbsp;&nbsp;&nbsp;</span>CDS
+                                title:'Legend of gene modal<a href="./help.php#seqresult" style="margin-left:10px">[Help]</a>',
+                                content:'&nbsp;&nbsp;<span style="text-align:center;background-color: #878787">&nbsp;&nbsp;&nbsp;&nbsp;</span>&nbsp;Extended 3\'UTR<br><br>&nbsp;&nbsp;<span style="text-align:center;background-color:#9FE0F6">&nbsp;&nbsp;&nbsp;&nbsp;</span>&nbsp;AMB',//<br><br>&nbsp;&nbsp;<span style="text-align:center;background-color:#00ABD8">&nbsp;&nbsp;&nbsp;&nbsp;</span>CDS
                                 trigger:'hover',
                                 type:'html'
                             });
