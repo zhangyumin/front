3b24a9374b909144d8c1af2a9b2b994b19fc94c1
zhangyumin618@163.com <zhangyumin618@163.com>
2016-04-02 21:34:20 +0800

    change page title

--- help.php
+++ help.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-        <title>polyA browser</title>
+        <title>PlantAPA-Help</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="./css/flexible-grids.css" rel="stylesheet" type="text/css"/>
         <script src="./src/jquery-1.10.1.min.js"></script>
