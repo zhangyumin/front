4c5c3ab34e008f2de1e3f5b6cb7f8a44613158d7
zhangyumin618@163.com <zhangyumin618@163.com>
2016-04-02 21:31:08 +0800

    change page title

--- browse.php
+++ browse.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-        <title>Search</title>
+        <title>PlantAPA-Browse</title>
         <script src="./src/jquery-1.10.1.min.js"></script>
 
         <link href="./src/index.css" rel="stylesheet" type="text/css" />
