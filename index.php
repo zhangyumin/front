60a7d50b0641a143ccd7f93b83ab6c893f3e2c90
zhangyumin618@163.com <zhangyumin618@163.com>
2016-04-02 21:34:46 +0800

    change page title

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-        <title>PlantAPA-home</title>
+        <title>PlantAPA-Home</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="./css/flexible-grids.css" rel="stylesheet" type="text/css"/>
         <script src="./src/jquery-1.10.1.min.js"></script>        
