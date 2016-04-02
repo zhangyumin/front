6089a8acec484a69b34f0a0b115e7f8f79fa0a7f
zhangyumin618@163.com <zhangyumin618@163.com>
2016-04-02 21:58:10 +0800

    fix href

--- info.php
+++ info.php
@@ -44,6 +44,12 @@ and open the template in the editor.
             text-align: justify;
             word-spacing: 0px;
         }
+        p{
+            margin: 0px;
+        }
+        h6{
+            margin:1em 0 0 0;
+        }
     </style>
     <body>
         <?php include './navbar.php'; ?>
@@ -69,7 +75,7 @@ and open the template in the editor.
                                 <h3 class="panel-title">●&nbsp;  What is PlantAPA?</h3>
                             </div>
                             <div class="panel-body">
-                                <p>PlantAPA is a web server for query, visualization, and analysis of poly(A) sites in plants, which can profile heterogeneous cleavage sites and quantify expression pattern of poly(A) sites across different conditions. To date, PlantAPA provides the largest database of APA in plants, including rice, Arabidopsis, <i>Medicago truncatula</i>, and <i>Chlamydomonas reinhardtii</i> (see <a href="./search.php">datasets</a>).</p>
+                                <p>PlantAPA is a web server for query, visualization, and analysis of poly(A) sites in plants, which can profile heterogeneous cleavage sites and quantify expression pattern of poly(A) sites across different conditions. To date, PlantAPA provides the largest database of APA in plants, including rice, Arabidopsis, <i>Medicago truncatula</i>, and <i>Chlamydomonas reinhardtii</i> (see <a href="./index.php">datasets</a>).</p>
                             </div>
                         </div>
                     </div>
@@ -81,14 +87,14 @@ and open the template in the editor.
                                 <h3 class="panel-title">●&nbsp;  Getting started</h3>
                             </div>
                             <div class="panel-body">
-                                <h6>(1) Extracting poly(A) sites from uploaded sequences</h6>
+                                <h6 style="margin-top:0px;">(1) Extracting poly(A) sites from uploaded sequences</h6>
                                 <p>You can upload short reads, ESTs, or poly(A) site coordinates for poly(A) site extraction. Upon the complete of the pipeline, the extracted poly(A) sites can be visualized in PlantAPA, or pooled with PlantAPA stored poly(A) sites for APA analysis.</p>
                                 <p>To start a task for poly(A) site extraction, click <a href="./upload_option.php">here</a>. A task ID will be assigned for each task. You can retrieve the result of your task at our <a href="./task.php">Data Retrieval Page</a>. You can also view all tasks you started at the <a href="./task.php">Data Retrieval Page</a>. Click <a href="./help.php">here</a> for a step by step guide. You can browse a demo task <a href="./demo.php?method=trap">here</a>.</p>
                                 <h6>(2) Searching PlantAPA and exporting sequences</h6>
                                 <p>Search by a variety of keywords, such as gene ID, chromosome fragment, gene functions, GO term, and GO ID. Examples: <a href="./sequence_detail.php?species=arab&seq=AT1G02130">AT1G02130</a> (gene ID, Arabidopsis), also <a href="./search_result.php?method=fuzzy&keyword=floral&species=arab">floral</a> (any key word in the search box of menu bar), <a href="./sequence_detail.php?species=mtr&seq=Medtr2g105570">Medtr2g105570</a> (Medicago).</p>
                                 <p>After searching, the <a href="./search_result.php?method=fuzzy&keyword=floral&species=arab">result</a> page provides a list of poly(A) sites and a tool bar for exporting various kinds of sequences onto local computers for other analysis purpose. Particularly, poly(A) sites in previously overlooked genomic regions such as extended 3’ UTR regions, ambiguous regions, and intergenic regions are also listed.</p>
                                 <h6>(3) Browsing PACs in the PAC browser</h6>
-                                <p>You can have a quick access to the PAC browser by clicking the "<a href="./browse.php">PAC browse</a>" tab in the main menu or the “View” link in a <a href="./search_result.php?method=fuzzy&keyword=floral&species=arab">PAC list</a>. One or more data sets from each plant species can be quickly loaded and graphically browsed online, by selecting the checkboxes of data sets in the ‘Tracks’ panel. Examples: <a href="../jbrowse/?data=data/arab&loc=Chr5:16395507..16399467&tracks=Gene annotation,DNA,APAplant stored PAC">AT5G40910</a> (Arabidopsis), also <a href="../jbrowse/?data=data/mtr&loc=chr2:45520733..45526107&tracks=Gene annotation,DNA,APAplant stored PAC">Medtr2g105570</a> (Medicago).</p>
+                                <p>You can have a quick access to the PAC browser by clicking the "<a href="./browse.php">PAC browse</a>" tab in the main menu or the “View” link in a <a href="./search_result.php?method=fuzzy&keyword=floral&species=arab">PAC list</a>. One or more data sets from each plant species can be quickly loaded and graphically browsed online, by selecting the checkboxes of data sets in the ‘Tracks’ panel. Examples: <a href="../jbrowse/?data=data/arab&loc=Chr5:16395507..16399467&tracks=Gene%20annotation%2CDNA%2CPlantAPA%20stored%20PAC%2Coxt6_leaf_1%20PAT%20plus%20strand%2Coxt6_leaf_2%20PAT%20plus%20strand%2Coxt6_leaf_3%20PAT%20plus%20strand%2Coxt6_root_1%20PAT%20plus%20strand%2Coxt6_root_2%20PAT%20plus%20strand%2Coxt6_root_3%20PAT%20plus%20strand%2Cwt_leaf_1%20PAT%20plus%20strand%2Cwt_leaf_2%20PAT%20plus%20strand%2Cwt_leaf_3%20PAT%20plus%20strand%2Cwt_root_1%20PAT%20plus%20strand%2Cwt_root_2%20PAT%20plus%20strand%2Cwt_root_3%20PAT%20plus%20strand%2Cwt_seed_1%20PAT%20plus%20strand%2Cwt_seed_2%20PAT%20plus%20strand&highlight=">AT5G40910</a> (Arabidopsis), also <a href="../jbrowse/?data=data/mtr&loc=chr2:45520733..45526107&tracks=Gene%20annotation%2CDNA%2Cwt_leaf_1%20PAT%20plus%20strand%2CPlantAPA%20stored%20PAC&highlight=">Medtr2g105570</a> (Medicago).</p>
                                 <h6>(4) Quantification and visualization of PACs across different conditions</h6>
                                 <p>By following the web link on a particular PAC or gene, a user can inspect various graphics and detailed information of the PACs in a gene or in a intergenic region, such as gene/PAC sequence, poly(A) signals, and PAT distributions across diverse conditions in the PAC viewer module.</p>
                                 <h6>(5) Analysis of APA switching between two conditions</h6>
@@ -105,7 +111,7 @@ and open the template in the editor.
                             </div>
                             <div class="panel-body">
                                 <p>The <a href="./help.php">Help section</a> provides more information, and the links at the top of the page will let you launch different modules. The <a href=./download.php>Download page</a> lets you get links to data set(s) of interest.
-                                <br><br><br>PlantAPA was developed by the bioinformatics group at Xiamen University. If you have any questions or comments, please email to xhuister@xmu.edu.cn (Dr. Xiaohui Wu).</p>
+                                <br><br>PlantAPA was developed by the bioinformatics group at Xiamen University. If you have any questions or comments, please email to xhuister@xmu.edu.cn (Dr. Xiaohui Wu).</p>
                             </div>
                         </div>
                     </div>
