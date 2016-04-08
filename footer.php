08a8017ea16b8f1e872e87b45594f9f93ad81ea6
zhangyumin618@163.com <zhangyumin618@163.com>
2016-04-08 10:47:19 +0800

    fix css of footer

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<div class="copy-right" style="font-family: 'Times New Roman','Times','serif';font-size: 15px;background-color: #eee;color: #5db95b;margin-top:20px;border-top: 1px solid rgba(0,0,0,0.2)">
+<div class="copy-right" style="font-family: 'Times New Roman','Times','serif';font-size: 15px;background-color: #eee;color: #5db95b;margin-top:20px;border-top: 1px solid rgba(0,0,0,0.2);min-width: 1080px">
     
    <center>© Copyright BMI Lab 2005-2013<!-- <script type="text/javascript" src="http://Register.xmu.edu.cn/RecordJS.aspx?RecordID=U501011"></script> --></center>
    <center>
@@ -8,6 +8,6 @@
 		<a target="_blank" href="http://Register.xmu.edu.cn/Record.aspx?RecordID=U501011">U501011</a>
    </center>
 </div>
-<div class="address"style="font-family: 'Times New Roman','Times','serif';font-size: 15px;background-color: #eee;color:#5db95b;">
+<div class="address"style="font-family: 'Times New Roman','Times','serif';font-size: 15px;background-color: #eee;color:#5db95b;min-width: 1080px">
     <center>Xiamen University, Xiamen 361005 China. e-mail:xhuister@xmu.edu.cn</center>
 </div>
