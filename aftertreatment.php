d6cbb04059217f692978fc5e16bb24c3472fd59f
zhangyumin618@163.com <zhangyumin618@163.com>
2016-04-16 22:52:50 +0800

    merge table for analysis pac

--- aftertreatment.php
+++ aftertreatment.php
@@ -223,6 +223,20 @@
 //            file_put_contents("/var/www/front/searched/test.txt",$sysQry."\n".$usrQry);
              shell_exec($merge);
         }
+        $usr_selected = array();
+        $sys_selected = array();
+        foreach ($_SESSION['file_real'] as $key => $value) {
+            if(in_array($value, $_SESSION['sample'])){
+                array_push($usr_selected, $value);
+            }
+        }
+        $sys_selected = array_diff($_SESSION['sample'], $usr_selected);
+        if(count($usr_selected)>0){
+            mysql_query("drop table db_user.PAC_merge_".$_SESSION['analysis'].";");
+            $merge = "./src/perl/PAT_mergePAC.pl -smptbls 'db_user.PAC_".$_SESSION['file'].";db_server.t_".$_POST['species']."_pac' -reftbl db_user.PAC_".$_SESSION['file']." -smpcols '".implode(":", $usr_selected).";".  implode(":", $sys_selected)."' -otbl PAC_merge_".$_SESSION['analysis']." -udist 24 -conf ./src/r/db_2.xml";
+            shell_exec($merge);
+        }
+        
     if($_GET['method']=='degene'){
 //            echo "<script>alert('in it')</script>";
         if($_POST['degene_nor_method']=='none')
