92b5cbdfcb050bb8a32b72ccdc934f22799af876
zhangyumin618@163.com <zhangyumin618@163.com>
2016-05-19 13:35:45 +0800

    test

--- test3.php
+++ test3.php
@@ -1,19 +1,4 @@
-<html>
-    <head>
-        <script src="./src/jquery-1.10.1.min.js"></script>
-        <script src="./src/jquery.webui-popover.js"></script>
-        <link href="./src/jquery.webui-popover.css" rel="stylesheet" type="text/css"/>
-    </head>
-    <body>
-        <a>hahaha</a>
-        <script>
-            $('a').webuiPopover({
-                placement:'right',//值: auto,top,right,bottom,left,top-right,top-left,bottom-right,bottom-left
-                title:'Title',
-                content:'Content<a href="www.baidu.com">click</a>',
-                trigger:'hover',
-                type:'html'
-            });
-        </script>
-    </body>
-</html>
+<?php
+    $a = file_get_contents("/home/workstation/htsflow/conf/htsflow.ini");
+    var_dump($a);
+?>
