01b3adc68d66ddda76e074d590129d1c176c75ee
zhangyumin <zhangyumin618@163.com>
2016-05-27 10:12:22 +0800

    add new samples

--- index.php
+++ index.php
@@ -61,6 +61,10 @@ and open the template in the editor.
                         <div style="padding-left:20px;font-size: 15px;color: #333">
                             <h2 style="border-bottom:1px solid #5db95b;text-align: left;padding-left: 10px">What's new<a href="./changelog.php" style="background-color:#5db95b;color:#fff;padding: 4px 5px;float:right;font-size: 12px;text-align: center;vertical-align: middle;border-radius: 4px;">More</a></h2>
                             <div style="background-color:#eee;border-radius: 4px;padding: 10px">
+                                Version 1.2.1&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;2016-05-20
+                            <ul>
+                                <li>Add new poly(A) site datasets</li>
+                            </ul><br>
                                 Version 1.2.0&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;2016-03-28
                             <ul>
                                 <li>Update the web UI and extensively debugging</li>
@@ -68,10 +72,6 @@ and open the template in the editor.
                                 Version 1.1.9&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;2016-02-20
                             <ul>
                                 <li>Add new dataset for Chlamy</li>
-                            </ul><br>
-                                Version 1.1.8&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;2016-02-12
-                            <ul>
-                                <li>Add PAC analysis module</li>
                             </ul>
                             </div>
                         </div>
