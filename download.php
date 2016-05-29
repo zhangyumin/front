946e6f9ebfbf0a1c275e74baccaf6a5773d612dc
zhangyumin618@163.com <zhangyumin618@163.com>
2016-05-29 11:25:49 +0800

    fix bug

--- download.php
+++ download.php
@@ -643,7 +643,7 @@ and open the template in the editor.
                 <font color="#224055" ><b>Downloading demo data in PlantAPA</b></font>
             </h2>
             <table  class="bordertable">
-                <head>
+                <thead>
                 <tr>
                     <th>File</th>
                     <th>Description</th>
